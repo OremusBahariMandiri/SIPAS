718124f627cd0921abcca82c223df40453f40f3f
Jamas108 <jamasguitaris@gmail.com>
2026-08-13 17:11:06 +0700

    tte page

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/verify/tte/{token}', [App\Http\Controllers\VerifyTteController::class, 'show'])
+     ->name('verify.tte');
 
 Route::middleware(['auth'])->group(function () {
 
